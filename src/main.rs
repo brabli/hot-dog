dad3856628c81800123bb23804524245590fa178
brabli <67018167+brabli@users.noreply.github.com>
2025-01-12 02:01:54 +0000

    Use signal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use dioxus::{logger::tracing, prelude::*};
+use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const TAILWIND: Asset = asset!("/assets/tailwind.css");
@@ -17,14 +17,12 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    use_context_provider(|| TitleState("DOGGY".to_string()));
     use_context_provider(|| TitleState("HotDoggy!".to_string()));
     rsx! {
         document::Stylesheet { href: TAILWIND }
         document::Link { rel: "icon", href: FAVICON }
         Title {}
         DogView {}
-
     }
 }
 
@@ -34,7 +32,7 @@ fn App() -> Element {
 fn Title() -> Element {
     let title = use_context::<TitleState>();
     rsx! {
-        div { id: "title",
+        div { id: "title", class: "text-2xl",
             h1 { "{title.0}" }
         }
     }
@@ -43,27 +41,28 @@ fn Title() -> Element {
 #[component]
 fn DogView() -> Element {
     let btn_classes = "p-2 border border-black rounded-sm bg-zinc-100 w-full";
-    let mut img_src = use_signal(|| "".to_string());
-    let fetch_new = move |_| async move {
-        let res = reqwest::get("https://dog.ceo/api/breeds/image/random")
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
             .await
             .unwrap()
             .json::<DogApi>()
             .await
-            .unwrap();
-        img_src.set(res.message);
-    };
+            .unwrap()
+            .message
+    });
 
     rsx! {
         div { id: "dogview",
-            img {
-                class: "max-w-sm",
-                src: "{img_src}",
-            }
+            img { class: "max-w-sm", src: img_src.cloned().unwrap_or_default() }
         }
 
         div { id: "buttons", class: "flex justify-between max-w-md p-4 gap-4",
-            button { onclick: fetch_new, id: "skip", class: btn_classes, "Skip" }
+            button {
+                onclick: move |_| img_src.restart(),
+                id: "skip",
+                class: btn_classes,
+                "Skip"
+            }
             button { id: "save", class: btn_classes, "Save" }
         }
     }
